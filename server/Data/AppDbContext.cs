57249fb774e8b3beb951dd277712ff1c149ac8df
aboze1n <midozean123@gmail.com>
2025-11-23 18:52:26 +0200

    setup Identity, JWT, Swagger and apply initial migration

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
